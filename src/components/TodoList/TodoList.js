9276fda4d1e166926d9b041508f97045a8b34555
Tae Kim <tkim25@kennesaw.edu>
2018-09-20 15:49:02 -0400

    added redux react-redux redux-actions immutable

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -9,24 +9,23 @@ class TodoList extends Component {
     render() {
         const { todos, onToggle, onRemove } = this.props;
         const todoList = todos.map(
-            todo => (
-                <TodoItem
-                    key={todo.id}
-                    done={todo.done}
-                    onToggle={() => onToggle(todo.id)}
-                    onRemove={() => onRemove(todo.id)}
-                    >
-                    {todo.text}
-                </TodoItem>
-            )
+          todo => (
+            <TodoItem
+              key={todo.get('id')}
+              done={todo.get('done')}
+              onToggle={() => onToggle(todo.get('id'))}
+              onRemove={() => onRemove(todo.get('id'))}>
+              {todo.get('text')}
+            </TodoItem>
+          )
         );
-
+    
         return (
-            <div>
-                {todoList}
-            </div>
+          <div>
+            {todoList}
+          </div>
         );
-    }
+      }
 }
 
 export default TodoList;
